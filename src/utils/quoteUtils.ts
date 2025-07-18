4e7f73dc2f727de4101519b89ae9c7dcb054dcd0
SahasTechnologies <sahas.shimpi@outlook.com>
2025-07-18 10:14:21 +1000

    bug fix

--- src/utils/quoteUtils.ts
+++ src/utils/quoteUtils.ts
@@ -1,5 +1,5 @@
 // Utility to get the correct Quote of the Day iframe URL based on theme
-import { ThemeKey } from './theme';
+import { ThemeKey } from './theme.ts';
 
 // Hardcoded URLs for each theme/mode/type combination
 const quoteUrlDark: Record<string, string> = {
@@ -23,21 +23,21 @@ const quoteUrlDark: Record<string, string> = {
 
 const quoteUrlLight: Record<string, string> = {
   red: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ffeaea',
-  extremered: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fee2e2',
+  extremered: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fecaca',
   orange: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fff0d9',
-  extremeorange: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ffedd5',
+  extremeorange: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fed7aa',
   yellow: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fffbe6',
-  extremeyellow: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fef9c3',
+  extremeyellow: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fef08a',
   green: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=eaffea',
-  extremegreen: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=dcfce7',
+  extremegreen: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=bbf7d0',
   blue: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=eaf0ff',
-  extremeblue: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=dbeafe',
+  extremeblue: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=bfdbfe',
   purple: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=f3eaff',
-  extremepurple: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ede9fe',
+  extremepurple: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ddd6fe',
   pink: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ffeaf0',
-  extremepink: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fce7f3',
+  extremepink: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=fbcfe8',
   grey: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=f0f0f0',
-  extremegrey: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=f3f4f6',
+  extremegrey: 'https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=e5e7eb',
 };
 
 export function getQuoteOfTheDayUrl(theme: ThemeKey, themeType: 'normal' | 'extreme', effectiveMode: 'light' | 'dark') {
