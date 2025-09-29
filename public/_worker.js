ab02545920d276d2086f99f3ab7146b6e67c3b6c
SahasTechnologies <sahas.shimpi@outlook.com>
2025-09-29 18:03:51 +1000

    redirect change

--- public/_worker.js
+++ public/_worker.js
@@ -2,13 +2,12 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    if (url.hostname.endsWith('.pages.dev')) {
+    if (url.hostname === "react-schoolplanner.pages.dev") {
       const newUrl = new URL(request.url);
-      newUrl.hostname = 'school.sahas.dpdns.org';
+      newUrl.hostname = "school.sahas.dpdns.org";
       return Response.redirect(newUrl.toString(), 301);
     }
 
-    // Fallback to the default Pages behavior
     return env.ASSETS.fetch(request);
   },
 };
